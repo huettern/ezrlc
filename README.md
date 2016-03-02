ffa15f86c4aba6ad5a602c584a1dc185fb227033
Noah Huetter <noahhuetter@gmail.com>
2016-03-02 11:01:19 +0100

    Added JFreeChart library
    Signed-off-by: Noah Huetter <noahhuetter@gmail.com>

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ Help -> Install new Software -> Work with..
 URL: http://download.eclipse.org/technology/m2e/releases/
 Press enter
 Select all and install.
+
+## Install jFreeChart in Eclipse
+
+Using JCommon 1.0.23 and JFreeChart 1.0.19 
+
+1. Download [JCommon](https://sourceforge.net/projects/jfreechart/files/) and [JFreeChart](https://sourceforge.net/projects/jfreechart/files/)
+2. Unzip both in any diretory
+3. Run `ant javadoc` in both `jcommon-1.0.23/and` and `jfreechart-1.0.19/ant` to generate javadocs
+4. In Eclipse, select Project Preferences, then choose the Java ->
+Build Path -> Libraries
+5. Add JARs.. and select the .jar file from the lib directory within the project
+6. Expand the library entrty, double click Source attachement, and add the external folder src from the extracted archive
+7. Repeat for JFreeChart
+
+## Installing ant
+### On Mac
+`brew install ant`
--- java/pro2/src/pro2/Pro2.java
+++ java/pro2/src/pro2/Pro2.java
@@ -5,6 +5,11 @@ package pro2;
 
 import java.io.IOException;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
 /**
  * @author noah
  *
@@ -23,6 +28,29 @@ public class Pro2 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
+		
+		/* JFreeChart test
+		 * 
+		 */
+		// create a dataset...
+		DefaultPieDataset datachart = new DefaultPieDataset();
+		datachart.setValue("Category 1", 43.2);
+		datachart.setValue("Category 2", 27.9);
+		datachart.setValue("Category 3", 79.5);
+		// create a chart...
+		JFreeChart chart = ChartFactory.createPieChart(
+				"Sample Pie Chart",
+				datachart,
+				true, // legend?
+				true, // tooltips?
+				false // URLs?
+				);
+		// create and display a frame...
+		ChartFrame frame = new ChartFrame("First", chart);
+		frame.pack();
+		frame.setVisible(true);
 		
 
 	}
