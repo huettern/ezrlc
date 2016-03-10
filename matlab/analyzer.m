c4d2fec587d6bf6a78c089a982c453488a7a15fc
Noah Huetter <noahhuetter@gmail.com>
2016-03-10 21:38:22 +0100

    Added a compare function to verify the Java routine
    Signed-off-by: Noah Huetter <noahhuetter@gmail.com>

--- matlab/analyzer.m
+++ matlab/analyzer.m
@@ -2,37 +2,48 @@
 %
 % EXAMPLE :
 % IN.file = '../sample_files/bsp1.s1p';
+% IN.filecomp = '../sample_files/bsp1.s1p.tmp';
 % 
-% 
+addpath 'incomming/s-param_toolbox/sbox'
+
+IN.inFilenmbr=1;
+IN.file = ['../sample_files/bsp' int2str(IN.inFilenmbr) '.s1p'];
+IN.filecomp = ['../sample_files/bsp' int2str(IN.inFilenmbr) '.s1p.tmp'];
 
 % read Datafile
 fid_log = fopen('SXPParse_log.txt','w');
 [freq, data, freq_noise, data_noise, Zo] = SXPParse(IN.file, fid_log);
 
+A = load(IN.filecomp);
+A = A(:,1) + A(:,2)*j;
+
+
+data = s2z(data);
+data2=data(:); % Data to compare
 
 % Extract data from one port
-parsed = zeros(length(data),1);
-for k = 1 : length(data)
-    parsed(k) = data(:,:,k);
-end
-
+parsed = data(:);
 
 
 % Plotting
 subplot(221)
-semilogx(freq,abs(parsed))
+semilogx(freq,abs(parsed),'+-b')
+semilogx(freq,abs(A),'*-r')
 title('magnitude')
 grid on
 
-subplot(223)
+subplot(222)
 semilogx(freq,angle(parsed))
 title('phase')
 grid on
 
-subplot(222)
+subplot(2,2,[3 4])
+plot(freq,abs(A)-abs(parsed),'*-r')
+title('deviation between MATLAB and Java')
+grid on;
 %smithchart(parsed)
 
-
+MaxDeviation=max(abs(A)-abs(parsed))
 
 % This only works with RF toolbox
 % data = read(rfdata.data,'../sample_files/bsp1.s1p');
