7db45a429197dce1462d2347a8e53fab7a6c5e4c
Jan Stocker <janstocker@gmx.ch>
2016-03-16 22:08:56 +0100

    look and feel -> crossplattform

--- java/pro2/src/pro2/FileChooser.java
+++ java/pro2/src/pro2/FileChooser.java
@@ -5,14 +5,17 @@ import java.io.File;
 import javax.accessibility.Accessible;
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
+import javax.swing.UIManager;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class FileChooser extends JComponent implements Accessible{
 	
+	//================================================================================
+    // local variables
+    //================================================================================
 	JFileChooser fileChooser = new JFileChooser();
 	private File file;
-	public String filename;
 	
 	//================================================================================
     // Constructors
@@ -22,18 +25,24 @@ public class FileChooser extends JComponent implements Accessible{
 	}
 	
 	public String windowFileChooser() {
-		int returnVal = fileChooser.showOpenDialog(null);
+		try {
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (Exception exception) { 
+			exception.printStackTrace(); 
+		}
+		
+		int returnVal = fileChooser.showDialog(null, "Open File");
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
             file = fileChooser.getSelectedFile();
 		}
-		return filename = file.getName();
+		return file.getName();
  	}
 	
 	public void fileFilter() {
 		FileNameExtensionFilter fileFilter = new FileNameExtensionFilter(
 		        "Touchstone File Format", "s1p", "z1p", "y1p");
 		fileChooser.setFileFilter(fileFilter);
+		
 	}
-	
 
 }
