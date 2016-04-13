e6d5a58f4942fafbb1f039fc07ee924b2efd3abe
Jan Stocker <Jan Stocker>
2016-04-13 15:16:13 +0200

    Add Measurement / Setting window

---
+++ java/pro2/src/pro2/Plot/RectPlotAddMeasurementWindow.java
@@ -0,0 +1,209 @@
+package pro2.Plot;
+
+import javax.swing.JDialog;
+
+import pro2.MVC.Controller;
+import java.awt.GridBagLayout;
+import javax.swing.JPanel;
+import java.awt.GridBagConstraints;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.Insets;
+import javax.swing.JRadioButton;
+import javax.swing.border.TitledBorder;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+
+public class RectPlotAddMeasurementWindow {
+
+	private JDialog dialog;
+
+	//================================================================================
+    // Constructors
+    //================================================================================
+
+	/**
+	 * @wbp.parser.entryPoint
+	 */	
+	public RectPlotAddMeasurementWindow(Controller controller) {
+		dialog = new JDialog(controller.getMainView());
+		dialog.setResizable(false);
+		dialog.setTitle("Add Measurement");		
+		dialog.setModal(true);
+		dialog.setLocation(250, 150);
+		dialog.setSize(300, 350);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 45, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+		dialog.getContentPane().setLayout(gridBagLayout);
+		
+		JPanel pnlDataSource = new JPanel();
+		pnlDataSource.setBorder(new TitledBorder(null, "Data Source", TitledBorder.CENTER, TitledBorder.TOP, null, null));
+		pnlDataSource.setToolTipText("");
+		pnlDataSource.setName("");
+		GridBagConstraints gbc_pnlDataSource = new GridBagConstraints();
+		gbc_pnlDataSource.insets = new Insets(0, 5, 5, 5);
+		gbc_pnlDataSource.fill = GridBagConstraints.BOTH;
+		gbc_pnlDataSource.gridx = 0;
+		gbc_pnlDataSource.gridy = 0;
+		dialog.getContentPane().add(pnlDataSource, gbc_pnlDataSource);
+		GridBagLayout gbl_pnlDataSource = new GridBagLayout();
+		gbl_pnlDataSource.columnWidths = new int[]{0, 0, 0};
+		gbl_pnlDataSource.rowHeights = new int[]{0, 0, 0};
+		gbl_pnlDataSource.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_pnlDataSource.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		pnlDataSource.setLayout(gbl_pnlDataSource);
+		
+		JRadioButton rdbtnFile = new JRadioButton("File:");
+		GridBagConstraints gbc_rdbtnFile = new GridBagConstraints();
+		gbc_rdbtnFile.anchor = GridBagConstraints.WEST;
+		gbc_rdbtnFile.insets = new Insets(0, 0, 5, 5);
+		gbc_rdbtnFile.gridx = 0;
+		gbc_rdbtnFile.gridy = 0;
+		pnlDataSource.add(rdbtnFile, gbc_rdbtnFile);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 0;
+		pnlDataSource.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JRadioButton rdbtnModel = new JRadioButton("Model:");
+		GridBagConstraints gbc_rdbtnModel = new GridBagConstraints();
+		gbc_rdbtnModel.anchor = GridBagConstraints.WEST;
+		gbc_rdbtnModel.insets = new Insets(0, 0, 0, 5);
+		gbc_rdbtnModel.gridx = 0;
+		gbc_rdbtnModel.gridy = 1;
+		pnlDataSource.add(rdbtnModel, gbc_rdbtnModel);
+		
+		JComboBox comboBox = new JComboBox();
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 1;
+		gbc_comboBox.gridy = 1;
+		pnlDataSource.add(comboBox, gbc_comboBox);
+		
+		JPanel pnlMeasurementType = new JPanel();
+		pnlMeasurementType.setBorder(new TitledBorder(null, "Measurement Type", TitledBorder.CENTER, TitledBorder.TOP, null, null));
+		GridBagConstraints gbc_pnlMeasurementType = new GridBagConstraints();
+		gbc_pnlMeasurementType.insets = new Insets(0, 5, 5, 5);
+		gbc_pnlMeasurementType.fill = GridBagConstraints.BOTH;
+		gbc_pnlMeasurementType.gridx = 0;
+		gbc_pnlMeasurementType.gridy = 1;
+		dialog.getContentPane().add(pnlMeasurementType, gbc_pnlMeasurementType);
+		GridBagLayout gbl_pnlMeasurementType = new GridBagLayout();
+		gbl_pnlMeasurementType.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_pnlMeasurementType.rowHeights = new int[]{0, 0};
+		gbl_pnlMeasurementType.columnWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_pnlMeasurementType.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		pnlMeasurementType.setLayout(gbl_pnlMeasurementType);
+		
+		JRadioButton rdbtnZ = new JRadioButton("Z");
+		GridBagConstraints gbc_rdbtnZ = new GridBagConstraints();
+		gbc_rdbtnZ.anchor = GridBagConstraints.WEST;
+		gbc_rdbtnZ.insets = new Insets(0, 0, 0, 5);
+		gbc_rdbtnZ.gridx = 0;
+		gbc_rdbtnZ.gridy = 0;
+		pnlMeasurementType.add(rdbtnZ, gbc_rdbtnZ);
+		
+		JRadioButton rdbtnS = new JRadioButton("S");
+		GridBagConstraints gbc_rdbtnS = new GridBagConstraints();
+		gbc_rdbtnS.anchor = GridBagConstraints.WEST;
+		gbc_rdbtnS.insets = new Insets(0, 0, 0, 5);
+		gbc_rdbtnS.gridx = 1;
+		gbc_rdbtnS.gridy = 0;
+		pnlMeasurementType.add(rdbtnS, gbc_rdbtnS);
+		
+		JRadioButton rdbtnY = new JRadioButton("Y");
+		GridBagConstraints gbc_rdbtnY = new GridBagConstraints();
+		gbc_rdbtnY.anchor = GridBagConstraints.WEST;
+		gbc_rdbtnY.gridx = 2;
+		gbc_rdbtnY.gridy = 0;
+		pnlMeasurementType.add(rdbtnY, gbc_rdbtnY);
+		
+		JPanel pnlComplexModifier = new JPanel();
+		pnlComplexModifier.setBorder(new TitledBorder(null, "Complex Modifier", TitledBorder.CENTER, TitledBorder.TOP, null, null));
+		GridBagConstraints gbc_pnlComplexModifier = new GridBagConstraints();
+		gbc_pnlComplexModifier.insets = new Insets(0, 5, 5, 5);
+		gbc_pnlComplexModifier.fill = GridBagConstraints.BOTH;
+		gbc_pnlComplexModifier.gridx = 0;
+		gbc_pnlComplexModifier.gridy = 2;
+		dialog.getContentPane().add(pnlComplexModifier, gbc_pnlComplexModifier);
+		GridBagLayout gbl_pnlComplexModifier = new GridBagLayout();
+		gbl_pnlComplexModifier.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_pnlComplexModifier.rowHeights = new int[]{0, 0, 0};
+		gbl_pnlComplexModifier.columnWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_pnlComplexModifier.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		pnlComplexModifier.setLayout(gbl_pnlComplexModifier);
+		
+		JRadioButton rdbtnReal = new JRadioButton("Real");
+		GridBagConstraints gbc_rdbtnReal = new GridBagConstraints();
+		gbc_rdbtnReal.anchor = GridBagConstraints.WEST;
+		gbc_rdbtnReal.insets = new Insets(0, 0, 5, 5);
+		gbc_rdbtnReal.gridx = 0;
+		gbc_rdbtnReal.gridy = 0;
+		pnlComplexModifier.add(rdbtnReal, gbc_rdbtnReal);
+		
+		JRadioButton rdbtnImag = new JRadioButton("Imag");
+		GridBagConstraints gbc_rdbtnImag = new GridBagConstraints();
+		gbc_rdbtnImag.anchor = GridBagConstraints.WEST;
+		gbc_rdbtnImag.insets = new Insets(0, 0, 5, 5);
+		gbc_rdbtnImag.gridx = 1;
+		gbc_rdbtnImag.gridy = 0;
+		pnlComplexModifier.add(rdbtnImag, gbc_rdbtnImag);
+		
+		JRadioButton rdbtnMag = new JRadioButton("Mag");
+		GridBagConstraints gbc_rdbtnMag = new GridBagConstraints();
+		gbc_rdbtnMag.insets = new Insets(0, 0, 5, 0);
+		gbc_rdbtnMag.anchor = GridBagConstraints.WEST;
+		gbc_rdbtnMag.gridx = 2;
+		gbc_rdbtnMag.gridy = 0;
+		pnlComplexModifier.add(rdbtnMag, gbc_rdbtnMag);
+		
+		JRadioButton rdbtnAngle = new JRadioButton("Angle");
+		GridBagConstraints gbc_rdbtnAngle = new GridBagConstraints();
+		gbc_rdbtnAngle.insets = new Insets(0, 0, 0, 5);
+		gbc_rdbtnAngle.anchor = GridBagConstraints.WEST;
+		gbc_rdbtnAngle.gridx = 0;
+		gbc_rdbtnAngle.gridy = 1;
+		pnlComplexModifier.add(rdbtnAngle, gbc_rdbtnAngle);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(0, 5, 5, 5);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 3;
+		dialog.getContentPane().add(panel, gbc_panel);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCancel.setBounds(152, 0, 65, 23);
+		panel.add(btnCancel);
+		
+		JButton btnOk = new JButton("OK");
+		btnOk.setBounds(227, 0, 47, 23);
+		panel.add(btnOk);
+	}
+	
+	//================================================================================
+    // Public Functions
+    //================================================================================
+	public void show() {
+		// TODO Auto-generated method stub
+		dialog.setVisible(true);
+	}
+}
--- java/pro2/src/pro2/Plot/RectPlotSettingsWindow.java
+++ java/pro2/src/pro2/Plot/RectPlotSettingsWindow.java
@@ -12,6 +12,11 @@ import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JSpinner;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import java.awt.Color;
 
 public class RectPlotSettingsWindow {
 	
@@ -31,30 +36,52 @@ public class RectPlotSettingsWindow {
 		dialog.setModal(true);
 		dialog.setLocation(250, 150);
 		dialog.setSize(300, 350);
-		dialog.getContentPane().setLayout(null);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{294, 0};
+		gridBagLayout.rowHeights = new int[]{322, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		dialog.getContentPane().setLayout(gridBagLayout);
 		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setToolTipText("");
-		tabbedPane.setBounds(0, 0, 294, 322);
-		dialog.getContentPane().add(tabbedPane);
+		JTabbedPane tabAxis = new JTabbedPane(JTabbedPane.TOP);
+		tabAxis.setToolTipText("");
+		GridBagConstraints gbc_tabAxis = new GridBagConstraints();
+		gbc_tabAxis.fill = GridBagConstraints.BOTH;
+		gbc_tabAxis.gridx = 0;
+		gbc_tabAxis.gridy = 0;
+		dialog.getContentPane().add(tabAxis, gbc_tabAxis);
 		
 		JPanel panel = new JPanel();
-		panel.setMaximumSize(new Dimension(0, 0));
-		tabbedPane.addTab("Axis", null, panel, null);
-		tabbedPane.setEnabledAt(0, true);
+		tabAxis.addTab("Axis", null, panel, null);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.rowHeights = new int[] {20, 20, 20, 20, 20, 20};
-		gbl_panel.columnWidths = new int[] {130, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+		gbl_panel.columnWidths = new int[]{0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 30, 0};
+		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{1.0, 1.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
-		JLabel lblXMin = new JLabel("X Min");
-		GridBagConstraints gbc_lblXMin = new GridBagConstraints();
-		gbc_lblXMin.insets = new Insets(0, 0, 5, 5);
-		gbc_lblXMin.gridx = 0;
-		gbc_lblXMin.gridy = 0;
-		panel.add(lblXMin, gbc_lblXMin);
+		JPanel pnlX = new JPanel();
+		pnlX.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "X-Axis", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		GridBagConstraints gbc_pnlX = new GridBagConstraints();
+		gbc_pnlX.insets = new Insets(5, 5, 0, 5);
+		gbc_pnlX.fill = GridBagConstraints.BOTH;
+		gbc_pnlX.gridx = 0;
+		gbc_pnlX.gridy = 0;
+		panel.add(pnlX, gbc_pnlX);
+		GridBagLayout gbl_pnlX = new GridBagLayout();
+		gbl_pnlX.columnWidths = new int[]{0, 0, 0};
+		gbl_pnlX.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_pnlX.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_pnlX.rowWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
+		pnlX.setLayout(gbl_pnlX);
+		
+		JLabel lblMinimum = new JLabel("Minimum:");
+		GridBagConstraints gbc_lblMinimum = new GridBagConstraints();
+		gbc_lblMinimum.anchor = GridBagConstraints.WEST;
+		gbc_lblMinimum.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMinimum.gridx = 0;
+		gbc_lblMinimum.gridy = 0;
+		pnlX.add(lblMinimum, gbc_lblMinimum);
 		
 		JSpinner spinner = new JSpinner();
 		GridBagConstraints gbc_spinner = new GridBagConstraints();
@@ -62,14 +89,15 @@ public class RectPlotSettingsWindow {
 		gbc_spinner.insets = new Insets(0, 0, 5, 0);
 		gbc_spinner.gridx = 1;
 		gbc_spinner.gridy = 0;
-		panel.add(spinner, gbc_spinner);
+		pnlX.add(spinner, gbc_spinner);
 		
-		JLabel lblXMax = new JLabel("X Max");
-		GridBagConstraints gbc_lblXMax = new GridBagConstraints();
-		gbc_lblXMax.insets = new Insets(0, 0, 5, 5);
-		gbc_lblXMax.gridx = 0;
-		gbc_lblXMax.gridy = 1;
-		panel.add(lblXMax, gbc_lblXMax);
+		JLabel lblMaximum = new JLabel("Maximum:");
+		GridBagConstraints gbc_lblMaximum = new GridBagConstraints();
+		gbc_lblMaximum.anchor = GridBagConstraints.WEST;
+		gbc_lblMaximum.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMaximum.gridx = 0;
+		gbc_lblMaximum.gridy = 1;
+		pnlX.add(lblMaximum, gbc_lblMaximum);
 		
 		JSpinner spinner_1 = new JSpinner();
 		GridBagConstraints gbc_spinner_1 = new GridBagConstraints();
@@ -77,67 +105,101 @@ public class RectPlotSettingsWindow {
 		gbc_spinner_1.insets = new Insets(0, 0, 5, 0);
 		gbc_spinner_1.gridx = 1;
 		gbc_spinner_1.gridy = 1;
-		panel.add(spinner_1, gbc_spinner_1);
+		pnlX.add(spinner_1, gbc_spinner_1);
 		
-		JLabel lblXSteps = new JLabel("X Steps");
-		GridBagConstraints gbc_lblXSteps = new GridBagConstraints();
-		gbc_lblXSteps.insets = new Insets(0, 0, 5, 5);
-		gbc_lblXSteps.gridx = 0;
-		gbc_lblXSteps.gridy = 2;
-		panel.add(lblXSteps, gbc_lblXSteps);
+		JLabel lblSteps = new JLabel("Steps:");
+		GridBagConstraints gbc_lblSteps = new GridBagConstraints();
+		gbc_lblSteps.anchor = GridBagConstraints.WEST;
+		gbc_lblSteps.insets = new Insets(0, 0, 0, 5);
+		gbc_lblSteps.gridx = 0;
+		gbc_lblSteps.gridy = 2;
+		pnlX.add(lblSteps, gbc_lblSteps);
 		
 		JSpinner spinner_2 = new JSpinner();
 		GridBagConstraints gbc_spinner_2 = new GridBagConstraints();
 		gbc_spinner_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_spinner_2.insets = new Insets(0, 0, 5, 0);
 		gbc_spinner_2.gridx = 1;
 		gbc_spinner_2.gridy = 2;
-		panel.add(spinner_2, gbc_spinner_2);
+		pnlX.add(spinner_2, gbc_spinner_2);
 		
-		JLabel lblYMin = new JLabel("Y Min");
-		GridBagConstraints gbc_lblYMin = new GridBagConstraints();
-		gbc_lblYMin.insets = new Insets(0, 0, 5, 5);
-		gbc_lblYMin.gridx = 0;
-		gbc_lblYMin.gridy = 3;
-		panel.add(lblYMin, gbc_lblYMin);
+		JPanel pnlY = new JPanel();
+		pnlY.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Y-Axis", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		GridBagConstraints gbc_pnlY = new GridBagConstraints();
+		gbc_pnlY.insets = new Insets(5, 5, 5, 5);
+		gbc_pnlY.fill = GridBagConstraints.BOTH;
+		gbc_pnlY.gridx = 0;
+		gbc_pnlY.gridy = 1;
+		panel.add(pnlY, gbc_pnlY);
+		GridBagLayout gbl_pnlY = new GridBagLayout();
+		gbl_pnlY.columnWidths = new int[]{0, 0, 0};
+		gbl_pnlY.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_pnlY.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_pnlY.rowWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
+		pnlY.setLayout(gbl_pnlY);
+		
+		JLabel lblMinimum_1 = new JLabel("Minimum:");
+		GridBagConstraints gbc_lblMinimum_1 = new GridBagConstraints();
+		gbc_lblMinimum_1.anchor = GridBagConstraints.WEST;
+		gbc_lblMinimum_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMinimum_1.gridx = 0;
+		gbc_lblMinimum_1.gridy = 0;
+		pnlY.add(lblMinimum_1, gbc_lblMinimum_1);
 		
 		JSpinner spinner_3 = new JSpinner();
 		GridBagConstraints gbc_spinner_3 = new GridBagConstraints();
 		gbc_spinner_3.fill = GridBagConstraints.HORIZONTAL;
 		gbc_spinner_3.insets = new Insets(0, 0, 5, 0);
 		gbc_spinner_3.gridx = 1;
-		gbc_spinner_3.gridy = 3;
-		panel.add(spinner_3, gbc_spinner_3);
+		gbc_spinner_3.gridy = 0;
+		pnlY.add(spinner_3, gbc_spinner_3);
 		
-		JLabel lblYMax = new JLabel("Y Max");
-		GridBagConstraints gbc_lblYMax = new GridBagConstraints();
-		gbc_lblYMax.insets = new Insets(0, 0, 5, 5);
-		gbc_lblYMax.gridx = 0;
-		gbc_lblYMax.gridy = 4;
-		panel.add(lblYMax, gbc_lblYMax);
+		JLabel lblMaximum_1 = new JLabel("Maximum:");
+		GridBagConstraints gbc_lblMaximum_1 = new GridBagConstraints();
+		gbc_lblMaximum_1.anchor = GridBagConstraints.WEST;
+		gbc_lblMaximum_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMaximum_1.gridx = 0;
+		gbc_lblMaximum_1.gridy = 1;
+		pnlY.add(lblMaximum_1, gbc_lblMaximum_1);
 		
 		JSpinner spinner_4 = new JSpinner();
 		GridBagConstraints gbc_spinner_4 = new GridBagConstraints();
 		gbc_spinner_4.fill = GridBagConstraints.HORIZONTAL;
 		gbc_spinner_4.insets = new Insets(0, 0, 5, 0);
 		gbc_spinner_4.gridx = 1;
-		gbc_spinner_4.gridy = 4;
-		panel.add(spinner_4, gbc_spinner_4);
+		gbc_spinner_4.gridy = 1;
+		pnlY.add(spinner_4, gbc_spinner_4);
 		
-		JLabel lblYSteps = new JLabel("Y Steps");
-		GridBagConstraints gbc_lblYSteps = new GridBagConstraints();
-		gbc_lblYSteps.insets = new Insets(0, 0, 0, 5);
-		gbc_lblYSteps.gridx = 0;
-		gbc_lblYSteps.gridy = 5;
-		panel.add(lblYSteps, gbc_lblYSteps);
+		JLabel lblSteps_1 = new JLabel("Steps:");
+		GridBagConstraints gbc_lblSteps_1 = new GridBagConstraints();
+		gbc_lblSteps_1.anchor = GridBagConstraints.WEST;
+		gbc_lblSteps_1.insets = new Insets(0, 0, 0, 5);
+		gbc_lblSteps_1.gridx = 0;
+		gbc_lblSteps_1.gridy = 2;
+		pnlY.add(lblSteps_1, gbc_lblSteps_1);
 		
 		JSpinner spinner_5 = new JSpinner();
 		GridBagConstraints gbc_spinner_5 = new GridBagConstraints();
-		gbc_spinner_5.weightx = 1.0;
 		gbc_spinner_5.fill = GridBagConstraints.HORIZONTAL;
 		gbc_spinner_5.gridx = 1;
-		gbc_spinner_5.gridy = 5;
-		panel.add(spinner_5, gbc_spinner_5);
+		gbc_spinner_5.gridy = 2;
+		pnlY.add(spinner_5, gbc_spinner_5);
+		
+		JPanel pnlButton = new JPanel();
+		pnlButton.setLayout(null);
+		GridBagConstraints gbc_pnlButton = new GridBagConstraints();
+		gbc_pnlButton.insets = new Insets(0, 5, 0, 0);
+		gbc_pnlButton.fill = GridBagConstraints.BOTH;
+		gbc_pnlButton.gridx = 0;
+		gbc_pnlButton.gridy = 2;
+		panel.add(pnlButton, gbc_pnlButton);
+		
+		JButton btnOk = new JButton("Ok");
+		btnOk.setBounds(183, 4, 89, 22);
+		pnlButton.add(btnOk);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.setBounds(83, 4, 89, 22);
+		pnlButton.add(btnCancel);
 	}
 
 	//================================================================================
@@ -147,5 +209,4 @@ public class RectPlotSettingsWindow {
 		// TODO Auto-generated method stub
 		dialog.setVisible(true);
 	}
-
 }
